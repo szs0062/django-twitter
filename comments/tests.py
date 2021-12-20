b0f4727833feb1fce98315cd19775fd4fe8c0954
sijun.shen <sijun.shen@bytedance.com>
2021-12-20 23:22:58 +0800

    temp

---
+++ comments/tests.py
@@ -0,0 +1,12 @@
+from testing.testcases import TestCase
+
+
+# Create your tests here.
+class CommentModelTests(TestCase):
+
+    def test_comment(self):
+        user = self.create_user('linghu')
+        tweet = self.create_tweet(user)
+        comment = self.create_comment(user, tweet)
+        self.assertEqual(comment.__str__, None)
+
