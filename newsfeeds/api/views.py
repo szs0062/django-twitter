1f3983702e310e18ae19c753740bc3f115a46080
sijun.shen <sijun.shen@bytedance.com>
2022-02-12 17:13:33 -0500

    complete cache newsfeed list in redis

--- newsfeeds/api/views.py
+++ newsfeeds/api/views.py
@@ -1,5 +1,6 @@
 from newsfeeds.api.serializers import NewsFeedSerializer
 from newsfeeds.models import NewsFeed
+from newsfeeds.services import NewsFeedService
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from utils.paginations import EndlessPagination
@@ -10,7 +11,7 @@ class NewsFeedViewSet(viewsets.GenericViewSet):
     pagination_class = EndlessPagination
 
     def list(self, request):
-        queryset = NewsFeed.objects.filter(user=self.request.user)
+        queryset = NewsFeedService.get_cached_newsfeeds(request.user.id)
         page = self.paginate_queryset(queryset)
         serializer = NewsFeedSerializer(
             page,
